7948614ba498c1927dc4453aca589c73dcf900f1
wael-saif23 <ws.mywork@gmail.com>
2024-02-27 00:06:13 +0200

    connected with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:chat_app_2/views/login_page.dart';
 import 'package:chat_app_2/views/register_page.dart';
 import 'package:flutter/material.dart';
-
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ChatApp2());
 }
 
